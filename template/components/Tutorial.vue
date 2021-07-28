2a54c38acfbab52fb713761e5d09ecde4c6e6658
Sam Nguyen <samnguyen@polk-county.net>
2021-07-28 12:48:54 -0400

    check in

---
+++ template/components/Tutorial.vue
@@ -0,0 +1,9 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <NuxtLink to="/about">
+      About (internal link that belongs to the Nuxt App)
+    </NuxtLink>
+  </div>
+</template>
