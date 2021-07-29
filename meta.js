9ab592f6dd6c62f840a952a07b01654100127bf0
Sam Nguyen <samnguyen@polk-county.net>
2021-07-29 15:49:28 -0400

    check in

--- meta.js
+++ meta.js
@@ -9,27 +9,34 @@ module.exports = {
     name: {
       type: 'string', // Question type
       required: true, // Is question Required
-      message: 'Project name' // Question it's self
+      message: 'Project name', // Question it's self
     },
     description: {
       type: 'string',
       required: false,
       message: '',
-      default: 'My Awesome Project'
+      default: 'My Awesome Project',
     },
     author: {
       type: 'string',
       message: 'Author',
-	  default: 'project@dev.com'
+      default: 'project@dev.com',
+      validate: function (answer) {
+        if (/@/g.test(answer)) {
+          return true;
+        }
+        return 'E-mail should contain the @ symbol';
+      },
     },
     extra: {
       type: 'string',
-      message: 'Extra Messaging!'
-    }
+      message: 'Extra Messaging!',
+    },
   },
 
   /**
    * You can add a custom complete message
    */
-  completeMessage: 'Project Complete!'
+  completeMessage:
+    'start the following steps, wait:  n  n {{^ inPlace}} cd {{destDirName}}  n {{/ inPlace}} npm i  n npm run dev or npm start',
 };
